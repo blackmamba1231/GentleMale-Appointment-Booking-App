b784840bb672c2750ac252cf3c09ad63a17e0b96
blackmamba1231 <sudhanshukumar321456@gmail.com>
2025-09-16 15:45:08 +0530

    Book-Appointment function done

--- apps/Backend/services/appointment.service.ts
+++ apps/Backend/services/appointment.service.ts
@@ -1,10 +1,23 @@
 import { db } from "db/client"
  
-export async function bookAppointment(input:{ service: String }, req: { user?: { id: string, role: 'CUSTOMER' | 'STYLIST' | 'ADMIN', sessionId: string }}){
+export async function bookAppointment(input:{ service: string, date: Date }, req: { user?: { id: string, role: 'CUSTOMER' | 'STYLIST' | 'ADMIN', sessionId: string }}){
     try{
         if(!req.user) throw new Error("UNAUTHENTICATED");
         if(req.user.role !== "CUSTOMER") throw new Error("FORBIDDEN");
-        
+        const user = await db.user.findUnique({ where: { id: req.user.id } });
+        if(!user) throw new Error("USER_NOT_FOUND");
+        const customerEmail = user.email;
+        const customerPhone = user.phoneE164;
+        const appointment = await db.appointment.create({
+            data: {
+                customerId: req.user.id,
+                service: input.service,
+                date: input.date,
+                customerEmail: customerEmail,
+                customerPhone: customerPhone
+            }
+        })
+        return appointment;
     } catch (e){
         throw e;
     }
