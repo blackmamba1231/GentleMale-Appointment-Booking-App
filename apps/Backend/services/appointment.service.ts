044cc3e5c7551ac57e892e8030d28423e37b5261
blackmamba1231 <sudhanshukumar321456@gmail.com>
2025-09-17 20:45:43 +0530

    Added email on confirmation

--- apps/Backend/services/appointment.service.ts
+++ apps/Backend/services/appointment.service.ts
@@ -1,4 +1,5 @@
 import { db } from "db/client"
+import { sendEmail } from "../utils/utils";
  
 export async function bookAppointment(input:{ service: string, date: Date }, req: { user?: { id: string, role: 'CUSTOMER' | 'STYLIST' | 'ADMIN', sessionId: string }}){
     try{
@@ -48,7 +49,7 @@ export async function cancelAppointment(params:{ id: string },req: { user?: { id
 export async function getAllAppointments(req: { user?: { id: string, role: 'CUSTOMER' | 'STYLIST' | 'ADMIN', sessionId: string }}){
     try{
         if(!req.user) throw new Error("UNAUTHENTICATED");
-        if(req.user.role !== "ADMIN") throw new Error("FORBIDDEN");
+        if(req.user.role !== "ADMIN" && req.user.role !== "STYLIST") throw new Error("FORBIDDEN");
         const appointments = await db.appointment.findMany({ where: { expired: false } });
         return appointments;
     } catch (e){
@@ -58,8 +59,10 @@ export async function getAllAppointments(req: { user?: { id: string, role: 'CUST
 export async function confirmAppointment(params:{ id: string },req: { user?: { id: string, role: 'CUSTOMER' | 'STYLIST' | 'ADMIN', sessionId: string }}){
     try{
         if(!req.user) throw new Error("UNAUTHENTICATED");
-        if(req.user.role !== "ADMIN") throw new Error("FORBIDDEN");
+        if(req.user.role !== "ADMIN" && req.user.role !== "STYLIST") throw new Error("FORBIDDEN");
         const appointment = await db.appointment.update({ where: { id: params.id }, data: { confirmed: true } });
+        const user = await db.user.findUnique({ where: { id: appointment.customerId } });
+        await sendEmail("Your appointment has been confirmed on " + appointment.date.toLocaleDateString() + " at " + appointment.date.toLocaleTimeString() + " with " + appointment.service + " service", user?.email!, "Your appointment has been confirmed");
         return appointment;
     } catch (e){
         throw e;
